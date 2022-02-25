88632bc9909ebc4e38eea55a120264e69e382d48
krasniqi rinor <rinor-1030@hotmail.com>
2022-02-25 02:36:42 +0100

    première partie

--- juste_prix_v2.py
+++ juste_prix_v2.py
@@ -3,17 +3,16 @@ from random import randint
 
 
 prix=randint(1, 100)
+tentative=10
+proposition_prix= int(input("bienvenue à vous, proposer un prix entre 1 et 100: "))
 
-tries=3
+while proposition_prix != prix:
+    tentative-=1
 
-proposition_prix= int(input("bienvenu à vous, proposer un prix entre 1 et 100: "))
-while tries > 0:
-    
-   while proposition_prix != prix:
-      if proposition_prix < prix:
+    if proposition_prix < prix:
           
           proposition_prix=int(input("c'est plus veuillez réecrire un prix: "))
     
-      elif proposition_prix>prix:
+    elif proposition_prix>prix:
           proposition_prix=int(input("c'est moins, veuillez réecrire un prix: "))
 print(f"félicitation vous avez trouver le juste prix qui est de {prix}")
